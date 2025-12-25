cc519f7ec37b69d8acd14ff96b561a8322c05c6e
Luca <luca.antonelli@gmx.ch>
2025-12-25 20:57:06 +0100

    chore(bump): fix font hack for new X11 font stuff

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -1,37 +1,31 @@
-{ pkgs, config, ... }:
+{ config, pkgs, ... }:
 
 {
-    system.fsPackages = [ pkgs.bindfs ];
-    fileSystems = let
-        mkRoSymBind = path: {
-        device = path;
-        fsType = "fuse.bindfs";
-        options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
-        };
-        aggregatedIcons = pkgs.buildEnv {
-        name = "system-icons";
-        paths = with pkgs; [
-            gnome-themes-extra
-        ];
-        pathsToLink = [ "/share/icons" ];
-        };
-        aggregatedFonts = pkgs.buildEnv {
-        name = "system-fonts";
-        paths = config.fonts.packages;
-        pathsToLink = [ "/share/fonts" ];
-        };
-    in {
-        "/usr/share/icons" = mkRoSymBind "${aggregatedIcons}/share/icons";
-        "/usr/local/share/fonts" = mkRoSymBind "${aggregatedFonts}/share/fonts";
+  system.fsPackages = [ pkgs.bindfs ];
+
+  fileSystems = let
+    mkRoSymBind = path: {
+      device = path;
+      fsType = "fuse.bindfs";
+      options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
     };
 
-    fonts = {
-        fontDir.enable = true;
-        packages = with pkgs; [
-        nerd-fonts.jetbrains-mono
-        nerd-fonts.noto
-        sn-pro
-        ];
+    aggregatedIcons = pkgs.buildEnv {
+      name = "system-icons";
+      paths = with pkgs; [ gnome-themes-extra ];
+      pathsToLink = [ "/share/icons" ];
     };
 
+    # IMPORTANT: use a runtime path to avoid infinite recursion.
+    # This directory is provided by the running system when fonts.fontDir.enable = true.
+    systemX11Fonts = "/run/current-system/sw/share/X11/fonts";
+  in {
+    "/usr/share/icons" = mkRoSymBind "${aggregatedIcons}/share/icons";
+    "/usr/local/share/fonts" = mkRoSymBind systemX11Fonts;
+  };
+
+  fonts = {
+    fontDir.enable = true;
+    packages = with pkgs; [ nerd-fonts.jetbrains-mono nerd-fonts.noto sn-pro ];
+  };
 }
