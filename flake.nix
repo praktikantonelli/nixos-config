200b2c30955dc5ba79672a8f6893ea77d832ed64
Luca <luca.antonelli@gmx.ch>
2025-12-26 11:14:28 +0100

    chore(nixvim): remove flake input as it's no longer used anywhere

--- flake.nix
+++ flake.nix
@@ -21,11 +21,6 @@
       submodules = true;
     };
 
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
