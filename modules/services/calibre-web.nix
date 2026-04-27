2538dd24ecface31a33c35a7cdc2987e385ab683
Luca Antonelli <luca.antonelli@gmx.ch>
2026-04-27 17:54:03 +0200

    fix(calibre-web): remove home protection to allow calibre to see /home

--- modules/services/calibre-web.nix
+++ modules/services/calibre-web.nix
@@ -1,20 +1,26 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   calibre-web-with-kobo = pkgs.calibre-web.overridePythonAttrs (oldAttrs: {
-    dependencies = oldAttrs.dependencies
-      ++ [ pkgs.calibre-web.optional-dependencies.kobo ];
+    dependencies = oldAttrs.dependencies ++ [ pkgs.calibre-web.optional-dependencies.kobo ];
   });
-in {
+in
+{
   services.calibre-web = {
     enable = true;
     package = calibre-web-with-kobo;
-    options.enableBookUploading = true;
-    options.enableKepubify = true;
+    options = {
+      enableBookUploading = true;
+      enableKepubify = true;
+      calibreLibrary = "/home/luca/library";
+    };
     user = "luca"; # allow access to /home/luca/library
+    group = "syncthing"; # allow using syncthing to sync library
     listen = {
       ip = "192.168.1.212";
       port = 8083;
     };
 
   };
+
+  systemd.services.calibre-web.serviceConfig.ProtectHome = lib.mkForce false;
 }
