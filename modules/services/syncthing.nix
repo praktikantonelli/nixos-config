b4365a31b11587c868116f8c9a5b067f52f573f1
Luca Antonelli <luca.antonelli@gmx.ch>
2026-06-12 23:35:16 +0200

    fix(syncthing): format code

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -1,15 +1,28 @@
-{ inputs, host, lib, ... }:
+{
+  inputs,
+  host,
+  lib,
+  ...
+}:
 let
   allDevices = {
-    "Desktop-Windows" = { id = inputs.secrets.syncthing-ids.windows; };
-    "phone" = { id = inputs.secrets.syncthing-ids.phone; };
-    "laptop" = { id = inputs.secrets.syncthing-ids.laptop; };
-    "homelab" = { id = inputs.secrets.syncthing-ids.homelab; };
+    "Desktop-Windows" = {
+      id = inputs.secrets.syncthing-ids.windows;
+    };
+    "phone" = {
+      id = inputs.secrets.syncthing-ids.phone;
+    };
+    "laptop" = {
+      id = inputs.secrets.syncthing-ids.laptop;
+    };
+    "homelab" = {
+      id = inputs.secrets.syncthing-ids.homelab;
+    };
   };
-  peers = lib.removeAttrs allDevices
-    [ host ]; # avoids referencing only other devices for folders
+  peers = lib.removeAttrs allDevices [ host ]; # avoids referencing only other devices for folders
   peerNames = builtins.attrNames peers;
-in {
+in
+{
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
