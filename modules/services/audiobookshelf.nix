08de8951a05ebcac40858af7f18d9b2f8615a38f
Luca Antonelli <luca.antonelli@gmx.ch>
2026-06-12 23:37:47 +0200

    feat(audiobookshelf): set data directory to `/srv`, create folder and assign permissions

--- modules/services/audiobookshelf.nix
+++ modules/services/audiobookshelf.nix
@@ -1,8 +1,13 @@
-{ ... }: {
+{ ... }:
+{
   services.audiobookshelf = {
     enable = true;
     host = "192.168.1.212";
     port = 8084;
     user = "luca";
   };
+
+  systemd.tmpfiles.rules = [
+    "d /srv/audiobooks 2775 syncthing media - -"
+  ];
 }
