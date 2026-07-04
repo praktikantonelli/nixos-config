bd7304da60c6364b2b5b46ef96a84c2d725e3d4b
Luca <luca.antonelli@gmx.ch>
2026-07-04 14:35:30 +0200

    fix(zellij): disable tips on startup

--- modules/home/zellij.nix
+++ modules/home/zellij.nix
@@ -2,6 +2,9 @@
   programs.zellij = {
     enable = true;
     plugins = with pkgs.zellijPlugins; [ vim-zellij-navigator ];
-    settings = { theme = "gruvbox-dark"; };
+    settings = {
+      theme = "gruvbox-dark";
+      show_startup_tips = false;
+    };
   };
 }
