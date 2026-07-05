84b695de486a5393fcdf45ba247cbec6661c4575
Luca <luca.antonelli@gmx.ch>
2026-07-05 11:50:19 +0200

    feat(services): add service to automatically cache user for easy greetd login

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -15,4 +15,23 @@
     # don’t shutdown when power button is short-pressed
     HandlePowerKey = "ignore";
   };
+
+  systemd.services.cache-regreet-user = {
+    description = "Cache ${username} for ReGreet";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "accounts-daemon.service" ];
+    requires = [ "accounts-daemon.service" ];
+
+    serviceConfig.Type = "oneshot";
+
+    script = ''
+      ${pkgs.systemd}/bin/busctl call --system \
+        org.freedesktop.Accounts \
+        /org/freedesktop/Accounts \
+        org.freedesktop.Accounts \
+        CacheUser \
+        s \
+        ${username} 
+    '';
+  };
 }
