d3e8467e5db1db310ae1c30ed9c88a482d007664
Luca <luca.antonelli@gmx.ch>
2026-07-05 08:22:02 +0200

    fix(boot): adjust boot loader layout

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -1,24 +1,32 @@
 { pkgs, host, ... }: {
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.grub = {
-    enable = true;
-    device = "nodev";
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-    useOSProber = false;
-    # Add Windows boot entry and make it appear before NixOS
-    extraEntriesBeforeNixOS = true;
-    extraEntries = if host == "desktop" then ''
-      menuentry 'Windows Boot Manager (on /dev/nvme1n1p1)' --class windows --class os $menuentry_id_option 'osprober-efi-ABD2-3058' {
-        insmod part_gpt
-        insmod fat
-        search --no-floppy --fs-uuid --set=root ABD2-3058
-        chainloader /EFI/Microsoft/Boot/bootmgfw.efi
-      }
-    '' else
-      "";
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    loader = {
+      systemd-boot.enable = false;
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiSupport = true;
+        efiInstallAsRemovable = true;
+        useOSProber = false;
+        # Add Windows boot entry and make it appear before NixOS
+        extraEntriesBeforeNixOS = true;
+        extraEntries =
+          if host == "desktop" then
+            ''
+              menuentry 'Windows Boot Manager (on /dev/nvme1n1p1)' --class windows --class os $menuentry_id_option 'osprober-efi-ABD2-3058' {
+                insmod part_gpt
+                insmod fat
+                search --no-floppy --fs-uuid --set=root ABD2-3058
+                chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+              }
+            ''
+          else
+            "";
+      };
+      efi.canTouchEfiVariables = false;
+      efi.efiSysMountPoint = "/boot";
+    };
   };
-  boot.loader.efi.canTouchEfiVariables = false;
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
