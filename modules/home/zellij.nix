f64c3834ba049a0d0ce52f8b429cfefffe96f4fb
Luca <luca.antonelli@gmx.ch>
2026-07-05 08:12:54 +0200

    feat(zellij): add key bindings from Windows zellij config

--- modules/home/zellij.nix
+++ modules/home/zellij.nix
@@ -5,6 +5,231 @@
     settings = {
       theme = "gruvbox-dark";
       show_startup_tips = false;
+      focus_follows_mouse = true;
+      default_mode = "normal";
+      default_shell = "nu";
+      keybinds = {
+        _props.clear-defaults = true;
+
+        # key binds for normal mode
+        normal._children = [
+          {
+            bind = {
+              _args = [
+                "Ctrl Space"
+              ];
+              SwitchToMode = [ "Tmux" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "Ctrl c"
+              ];
+              Write = [ 3 ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "Ctrl d"
+              ];
+              Write = [ 4 ];
+            };
+          }
+        ];
+
+        # key binds for tmux mode
+        tmux._children = [
+          {
+            bind = {
+              _args = [
+                "Ctrl Space"
+                "Esc"
+              ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "Ctrl c"
+              ];
+              Write = [ 3 ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "Ctrl d"
+              ];
+              Write = [ 4 ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "\""
+              ];
+              NewPane = [ "Down" ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "%"
+              ];
+              NewPane = [ "Right" ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "c"
+              ];
+              NewTab = [ ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "x"
+              ];
+              CloseFocus = [ ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "X"
+              ];
+              CloseTab = [ ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "h"
+                "Left"
+              ];
+              MoveFocus = [
+                "Left"
+              ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "j"
+                "Down"
+              ];
+              MoveFocus = [
+                "Down"
+              ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "k"
+                "Up"
+              ];
+              MoveFocus = [
+                "Up"
+              ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "l"
+                "Right"
+              ];
+              MoveFocus = [
+                "Right"
+              ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "n"
+              ];
+              GoToNextTab = [ ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "p"
+              ];
+              GoToPreviousTab = [ ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "o"
+              ];
+              FocusNextPane = [ ];
+              SwitchToMode = [ "Normal" ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "1"
+              ];
+              GoToTab = [ 1 ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "2"
+              ];
+              GoToTab = [ 2 ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "3"
+              ];
+              GoToTab = [ 3 ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "4"
+              ];
+              GoToTab = [ 4 ];
+            };
+          }
+          {
+            bind = {
+              _args = [
+                "5"
+              ];
+              GoToTab = [ 5 ];
+            };
+          }
+        ];
+      };
     };
   };
 }
