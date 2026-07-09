eb81dba837277e4872a58e6c39c0dac1ce9e57ca
Luca <luca.antonelli@gmx.ch>
2026-07-09 22:13:08 +0200

    chore(tmux): remove tmux and imports

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -16,7 +16,6 @@
     ./starship.nix # shell prompt
     ./swaylock.nix # lock screen
     ./vscodium.nix # vscode forck
-    # ./tmux.nix # tmux
     ./zellij.nix
     ./waybar # status bar
     ./nu.nix # nushell
