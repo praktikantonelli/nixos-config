73fc96b5d22e75b8a7300ae57ba1713d545b0b75
Luca <luca.antonelli@gmx.ch>
2026-07-10 07:47:11 +0200

    feat(git): set up commit signing

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,8 +1,12 @@
-{ inputs, ... }:
+{ config, inputs, ... }:
 {
   programs.git = {
     enable = true;
-    signing.format = "openpgp";
+    signing = {
+      format = "ssh";
+      key = "~/.ssh/id_ed25519.pub";
+      signByDefault = true;
+    };
 
     settings = {
       user = {
@@ -13,7 +17,12 @@
       init.defaultBranch = "main";
       credential.helper = "store";
       core.editor = "nvim";
+      gpg.ssh.allowedSignersFile = "${config.home.homeDirectory}/.ssh/allowed_signers";
     };
   };
+
+  home.file.".ssh/allowed_signers".text = ''
+    ${inputs.secrets.email} namespaces="git" ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEBJ8I33Kaq9DW9eWl2wAujokyM+QjqFA1hRgfBsAs9I ${inputs.secrets.email}
+  '';
 
 }
