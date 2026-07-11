21aaecdd256afde7f803c81d855c4d903a609558
Luca <luca.antonelli@gmx.ch>
2026-07-11 07:57:39 +0200

    feat(ssh): add laptop and desktop ssh public keys as authorized ssh keys for homelab

--- modules/core/homelab.nix
+++ modules/core/homelab.nix
@@ -1,11 +1,4 @@
-{
-  pkgs,
-  host,
-  username,
-  inputs,
-  ...
-}:
-{
+{ pkgs, host, username, inputs, ... }: {
   imports = [
     ./sops.nix # secrets management
     ../services/homelab.nix # definitions of systemd services for homelab
@@ -27,19 +20,13 @@
   users.users.${username} = {
     isNormalUser = true;
     description = "${username}";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-      "docker"
-    ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    openssh.authorizedKeys.keyFiles =
+      [ ../../hosts/desktop/ssh-key.pub ../../hosts/laptop/ssh-key.pub ];
     shell = pkgs.nushell;
   };
 
-  environment.systemPackages = with pkgs; [
-    tailscale
-    jdk8
-    nextcloud-client
-  ];
+  environment.systemPackages = with pkgs; [ tailscale jdk8 nextcloud-client ];
 
   services = {
     tailscale.enable = true;
@@ -60,43 +47,24 @@
       defaults.email = inputs.secrets.email;
       acceptTerms = true;
     };
-    sudo.extraRules = [
-      {
-        users = [ "luca" ];
-        commands = [
-          {
-            command = "ALL";
-            options = [ "NOPASSWD" ];
-          }
-        ];
-      }
-    ];
+    sudo.extraRules = [{
+      users = [ "luca" ];
+      commands = [{
+        command = "ALL";
+        options = [ "NOPASSWD" ];
+      }];
+    }];
   };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   # Enable experimental features
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [
-    22
-    80
-    180
-    443
-    1443
-    2283
-    8080
-    8083
-    8084
-    8222
-    8888
-    1234
-  ];
+  networking.firewall.allowedTCPPorts =
+    [ 22 80 180 443 1443 2283 8080 8083 8084 8222 8888 1234 ];
 
   zramSwap = {
     enable = true;
@@ -110,12 +78,10 @@
     auto-optimise-store = true;
   };
 
-  swapDevices = [
-    {
-      device = "/swapfile";
-      size = 8192;
-    }
-  ];
+  swapDevices = [{
+    device = "/swapfile";
+    size = 8192;
+  }];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
