7599d61105aacb72c356a59700d02bc29bfa359e
Luca <luca.antonelli@gmx.ch>
2026-07-13 21:32:10 +0100

    fix(hyprland): add missing `end` for lua function

--- modules/home/hyprland/config2.nix
+++ modules/home/hyprland/config2.nix
@@ -8,20 +8,21 @@
         _args = [
           "hyprland.start"
           (lib.generators.mkLuaInline ''
-                function()
-            hl.exec_cmd("systemctl --user import-environment")
-            hl.exec_cmd("hash dbus-update-activation-environment 2>/dev/null")
-            hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
-            hl.exec_cmd("nm-applet")
-            hl.exec_cmd("wl-clip-persist --clipboard both")
-            hl.exec_cmd("swaybg -m fill -i $(find ~/Pictures/wallpapers/ -maxdepth 1 -type f)")
-            hl.exec_cmd("hyprctl setcursor Nordzy-cursors 22")
-            hl.exec_cmd("hyprctl dispatch workspace 1")
-            hl.exec_cmd("poweralertd")
-            hl.exec_cmd("waybar")
-            hl.exec_cmd("swaync")
-            hl.exec_cmd("wl-paste --watch cliphist store")
-            hl.exec_cmd("owncloud")
+            function()
+              hl.exec_cmd("systemctl --user import-environment")
+              hl.exec_cmd("hash dbus-update-activation-environment 2>/dev/null")
+              hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+              hl.exec_cmd("nm-applet")
+              hl.exec_cmd("wl-clip-persist --clipboard both")
+              hl.exec_cmd("swaybg -m fill -i $(find ~/Pictures/wallpapers/ -maxdepth 1 -type f)")
+              hl.exec_cmd("hyprctl setcursor Nordzy-cursors 22")
+              hl.exec_cmd("hyprctl dispatch workspace 1")
+              hl.exec_cmd("poweralertd")
+              hl.exec_cmd("waybar")
+              hl.exec_cmd("swaync")
+              hl.exec_cmd("wl-paste --watch cliphist store")
+              hl.exec_cmd("owncloud")
+            end
           '')
         ];
       };
