f79251b8761bac5cb395628770673716e310c39b
Luca <luca.antonelli@gmx.ch>
2026-07-14 22:41:18 +0100

    fix(hyprland): include lua via extraLuaFiles option to silence warning

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -5,7 +5,4 @@
     inputs.hyprland.homeManagerModules.default
   ];
 
-  # stop fighting home manager to lua translations and just use lua directly
-  xdg.configFile."hypr/hyprland.lua".source = ./hyprland.lua;
-
 }
