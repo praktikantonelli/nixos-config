59c4dfdcb8eefcd311718df8f16af96f5c5ec570
Luca <luca.antonelli@gmx.ch>
2026-07-18 07:29:42 +0100

    docs(hyprland): add TODO comment for keybind script

--- modules/home/scripts/scripts/keybinds.sh
+++ modules/home/scripts/scripts/keybinds.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# TODO: adapt regex so it works with new lua config syntax
 config_file=~/.config/hypr/hyprland.conf
 keybinds=$(grep -oP '(?<=bind=).*' $config_file)
 keybinds=$(echo "$keybinds" | sed 's/,\([^,]*\)$/ = \1/' | sed 's/, exec//g' | sed 's/^,//g')
