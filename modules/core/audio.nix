59d2593fea535b92475a3b4fb19a023e81b40383
Luca <luca.antonelli@gmx.ch>
2026-07-19 22:21:42 +0200

    feat(audacity): add audacity

--- modules/core/audio.nix
+++ modules/core/audio.nix
@@ -7,5 +7,8 @@
     pulse.enable = true;
     # lowLatency.enable = true;
   };
-  environment.systemPackages = with pkgs; [ pulseaudioFull ];
+  environment.systemPackages = with pkgs; [
+    pulseaudioFull
+    audacity
+  ];
 }
