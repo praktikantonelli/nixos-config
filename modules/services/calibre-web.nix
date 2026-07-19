8d0f6386b974bc3d6dbf71c7beabeb0ea58512d4
Luca Antonelli <luca.antonelli@gmx.ch>
2026-07-19 10:09:50 +0200

    fix(calibre-web): add overlay to fix broken pip-chill dependency

--- modules/services/calibre-web.nix
+++ modules/services/calibre-web.nix
@@ -4,7 +4,30 @@ let
     dependencies = oldAttrs.dependencies
       ++ [ pkgs.calibre-web.optional-dependencies.kobo ];
   });
-in {
+in
+{
+
+  # calibre-web currently pulls in a broken version of pip-chill (1.0.3) -> override with 1.0.5 until fixed upstream
+  nixpkgs.overlays = [
+    (final: prev: {
+      pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+        (_pythonFinal: pythonPrev: {
+          pip-chill = pythonPrev.pip-chill.overridePythonAttrs (_oldAttrs: rec {
+            version = "1.0.5";
+
+            src = final.fetchPypi {
+              pname = "pip_chill";
+              inherit version;
+              hash = "sha256-55vFFKv+FE8u9SKQ9ZZ30nnLBbQIT6n4FLvlzA6gTBw=";
+            };
+
+            dependencies = [ ];
+          });
+        })
+      ];
+    })
+  ];
+
   services.calibre-web = {
     enable = true;
     package = calibre-web-with-kobo;
