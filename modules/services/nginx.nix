e7e99addfcff29ceaf74b515ca4e6ba57a0381ae
Luca <luca.antonelli@gmx.ch>
2026-07-22 13:55:15 +0200

    feat(nginx): properly set up nginx so it routes incoming requests from cloudflared to local services

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -1,17 +1,18 @@
 { inputs, config, ... }: {
   services.nginx = {
     enable = true;
+    defaultListen = [
+      {
+        addr = "127.0.0.1";
+        port = 80;
+      }
+    ];
+    recommendedProxySettings = true;
 
     virtualHosts = {
-      "nextcloud.${inputs.secrets.domain}" = {
-        listen = [{
-          addr = "192.168.1.212";
-          port = 8888;
-        }];
-      };
+      "nextcloud.${inputs.secrets.domain}" = { };
+
       "bitwarden.${inputs.secrets.domain}" = {
-        # enableACME = true;
-        # forceSSL = true;
         locations."/" = {
           proxyPass = "http://127.0.0.1:${
               toString config.services.vaultwarden.config.ROCKET_PORT
@@ -21,11 +22,44 @@
       };
 
       "immich.${inputs.secrets.domain}" = {
-        enableACME = false;
-        forceSSL = false;
         locations."/" = {
-          proxyPass =
-            "http://127.0.0.1:${toString config.services.immich.port}";
+          proxyPass = "http://${config.services.immich.host}:${
+              toString config.services.immich.port
+            }";
+          proxyWebsockets = true;
+        };
+      };
+
+      "calibre.${inputs.secrets.domain}" = {
+        locations."/" = {
+          proxyPass = "http://${config.services.calibre-web.listen.ip}:${
+              toString config.services.calibre-web.listen.port
+            }";
+          proxyWebsockets = true;
+        };
+      };
+
+      "audiobookshelf.${inputs.secrets.domain}" = {
+        locations."/" = {
+          proxyPass = "http://${config.services.audiobookshelf.host}:${
+              toString config.services.audiobookshelf.port
+            }";
+          proxyWebsockets = true;
+        };
+      };
+
+      "navidrome.${inputs.secrets.domain}" = {
+        locations."/" = {
+          proxyPass = "http://${config.services.navidrome.settings.Address}:${
+              toString config.services.navidrome.settings.Port
+            }";
+          proxyWebsockets = true;
+        };
+      };
+
+      "onlyoffice.${inputs.secrets.domain}" = {
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:180";
           proxyWebsockets = true;
         };
       };
