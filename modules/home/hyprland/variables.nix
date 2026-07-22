05f801312db2cb72b9109d9e77f8e8125e784d02
Luca <luca.antonelli@gmx.ch>
2026-07-22 16:38:55 +0200

    fix(hyprland): fix contradicting variables

--- modules/home/hyprland/variables.nix
+++ modules/home/hyprland/variables.nix
@@ -11,9 +11,7 @@
     WLR_DRM_NO_ATOMIC = "1";
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    QT_QPA_PLATFORM = "xcb";
-    QT_QPA_PLATFORMTHEME = "qt5ct";
-    QT_STYLE_OVERRIDE = "kvantum";
+    QT_QPA_PLATFORM = "wayland;xcb";
     MOZ_ENABLE_WAYLAND = "1";
     WLR_BACKEND = "vulkan";
     WLR_RENDERER = "vulkan";
