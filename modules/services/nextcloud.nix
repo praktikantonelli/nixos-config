18740718eefc8b5eaaae5ea99fb58b5ef074a2bd
Luca <luca.antonelli@gmx.ch>
2026-07-24 21:08:58 +0200

    fix(nextcloud): limit file chunk size to 50MB, according to nextcloud docs for cloudflare tunnels

--- modules/services/nextcloud.nix
+++ modules/services/nextcloud.nix
@@ -15,6 +15,9 @@
       adminpassFile = config.sops.secrets.nextcloud-admin-pass.path;
       dbtype = "mysql";
     };
+    settings = {
+      "files.chunked_upload.max_size" = 50000000;
+    };
     database.createLocally = true;
     configureRedis = true;
     maxUploadSize = "16G";
