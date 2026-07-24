746f8755c5cbb229fb86b4c1e8c917e8f841f9e9
Luca <luca.antonelli@gmx.ch>
2026-07-24 21:04:40 +0200

    fix(immich): move media location to /srv, will be on HDD after rebuild

--- modules/services/immich.nix
+++ modules/services/immich.nix
@@ -3,5 +3,13 @@
     enable = true;
     port = 2283;
     host = "192.168.1.212";
+    mediaLocation = "/srv/immich";
   };
+
+  # immich does not create this directory automatically
+  systemd.tmpfiles.rules = [
+    # create the directory and grant the immich user read, write and execute permissions
+    "d /srv/immich 0700 immich immich -"
+  ];
+
 }
