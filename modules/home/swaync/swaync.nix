d11c238f8ab5e2a13ab22bd85d3b4a097f17b56e
Luca <luca.antonelli@gmx.ch>
2026-08-15 13:57:54 +0200

    fix(grimblast): add options to toggle swayNC, save output also as image file

--- modules/home/swaync/swaync.nix
+++ modules/home/swaync/swaync.nix
@@ -1,5 +1,13 @@
-{ lib, ... }: {
+{ config, lib, ... }: {
   services.swaync.enable = true;
   xdg.configFile."swaync/style.css".source = ./style.css;
   xdg.configFile."swaync/config.json".source = lib.mkForce ./config.json;
+
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+
+    extraConfig.SCREENSHOTS = "${config.home.homeDirectory}/Pictures/Screenshots";
+    setSessionVariables = false;
+  };
 }
