df27f3c49da3274d95905b0cc649805e26804930
Luca <luca.antonelli@gmx.ch>
2026-08-24 20:14:19 +0200

    feat(zennotes): add nginx

--- modules/services/zennotes.nix
+++ modules/services/zennotes.nix
@@ -1,5 +1,15 @@
-{ inputs, pkgs, ... }: {
+{ inputs, pkgs, ... }:
+let
+  inherit (import ./nginx-proxy.nix) cloudflareProxy;
+in
+{
   environment.systemPackages = [
     inputs.zennotes.packages.${pkgs.system}.zennotes-server
   ];
+
+  services.nginx.virtualHosts."zennotes.${inputs.secrets.domain}" = {
+    locations."/" = cloudflareProxy {
+      proxyPass = "http://127.0.0.1:7878";
+    };
+  };
 }
