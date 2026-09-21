d9c783f5947afa90a186e244a6ba9d7912f5895f
Luca Antonelli <79634977+praktikantonelli@users.noreply.github.com>
2026-09-21 13:46:27 +0200

    feat(bookorbit): update to v3.0

--- modules/services/bookorbit.nix
+++ modules/services/bookorbit.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  appImage = "ghcr.io/bookorbit/bookorbit:2.10.0";
+  appImage = "ghcr.io/bookorbit/bookorbit:3.0.0";
   postgresImage = "pgvector/pgvector:pg16";
 
   contentRoot = "/srv/bookorbit";
